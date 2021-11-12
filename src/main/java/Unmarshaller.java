40894cb78032a28c0b8aca91aea00170d8c6e992
Benedikt Linse <benedikt@confluent.io>
2021-11-12 23:36:05 +0100

    Adding unmarshaller

---
+++ src/main/java/Unmarshaller.java
@@ -0,0 +1,20 @@
+import books.BooksForm;
+import books.ObjectFactory;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+
+public class Unmarshaller {
+
+    public void unmarshal() throws JAXBException, FileNotFoundException {
+        var jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+        javax.xml.bind.Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        BooksForm booksForm = jaxbUnmarshaller.unmarshal(
+                new StreamSource(new FileInputStream("src/main/resources/books.xml")),
+                BooksForm.class
+        ).getValue();
+        System.out.println(booksForm);
+    }
+}
---
+++ src/test/java/UnmarshallerTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
+
+public class UnmarshallerTest {
+
+    @Test
+    public void test() throws JAXBException, FileNotFoundException {
+        Unmarshaller xmlParser = new Unmarshaller();
+        xmlParser.unmarshal();
+    }
+}
