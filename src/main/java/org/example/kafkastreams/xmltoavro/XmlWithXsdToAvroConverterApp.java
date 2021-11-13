88b3a3e0cdf464bd3e002aff5d294fdbde0580f8
Benedikt Linse <benedikt@confluent.io>
2021-11-13 13:24:04 +0100

    serializing the books with the RefelctionAvroSerializer and writing the data to Kafka

--- src/main/java/org/example/kafkastreams/xmltoavro/XmlWithXsdToAvroConverterApp.java
+++ src/main/java/org/example/kafkastreams/xmltoavro/XmlWithXsdToAvroConverterApp.java
@@ -1,10 +1,16 @@
 package org.example.kafkastreams.xmltoavro;
 
+import books.BooksForm;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.ReflectionAvroSerde;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Produced;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +19,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collections;
 import java.util.Properties;
 
 @SpringBootApplication
@@ -47,6 +54,12 @@ public class XmlWithXsdToAvroConverterApp {
 		xmlStream.peek((k,v) -> log.info(new String(v)));
 		var bookStream = xmlStream.mapValues(unmarshaller::unmarshal);
 		bookStream.peek((k,v) -> log.info(v.toString()));
+		Serde<BooksForm> reflectionAvroSerde = new ReflectionAvroSerde<>();
+ 		reflectionAvroSerde.configure(
+				 Collections.singletonMap(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081/"),
+				false
+		);
+		bookStream.to("books-avro", Produced.with(Serdes.String(), reflectionAvroSerde));
 		var topology = streamsBuilder.build();
 		KafkaStreams kafkaStreams = new KafkaStreams(topology, kafkaStreamsProperties);
 		kafkaStreams.start();
